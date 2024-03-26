a08c036aff794dbcc1ae7646a309c367e37d819e
jakedoa <107153905+jakedoa@users.noreply.github.com>
2024-03-25 18:10:19 -0700

    Added supabase

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
-import React from "react";
-import Navigator from './src/components/routes/stack';
+import 'react-native-url-polyfill/auto'
+import { useState, useEffect } from 'react'
+import { supabase } from './lib/supabase'
+import Auth from './components/Auth'
+import Account from './screens/Account'
+import { View } from 'react-native'
+import { Session } from '@supabase/supabase-js'
 
-export default function App () {
+export default function App() {
+  const { session, setSession } = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
-    <Navigator/>
+    <View>
+      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+    </View>
   )
 }
 
