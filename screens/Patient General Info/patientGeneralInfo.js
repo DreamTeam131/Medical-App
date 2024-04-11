14c8a53eb6c04c6f41b07c7834077dc001979660
leefy010 <160988680+leefy010@users.noreply.github.com>
2024-04-10 23:11:15 -0700

    Add files via upload

---
+++ screens/Patient General Info/patientGeneralInfo.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {useState , useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { supabase } from '../../lib/supabase';
+import PatientCard from '../../lib/PatientCard';
+
+const PatientGeneralInfo = ({ navigation }) => {
+    const [ fetchError , setFetchError ] = useState(null)
+    const [ patient, setPatient ] = useState(null)
+    useEffect(() => {
+        const fetchPatient = async () => {
+            const { data , error } = await supabase
+            .from('userData')
+            .select()
+
+            if ( error ) {
+                setFetchError('Could not fetch patient')
+                setPatient(null)
+                console.log(error)
+            }
+            if ( data ) {
+                setPatient(data)
+                setFetchError(null)
+            }
+        }
+        fetchPatient()
+    } , [])
+
+    
+    return (
+    <View style = {styles.container} >
+        {fetchError && (<Text>{fetchError}</Text>)}
+        {patient && (
+            <View >
+                {patient.map(patient => (
+                    <Text>
+                        <PatientCard key = {patient.user_id} patient = {patient} />
+                    </Text>
+                    
+                ))}
+            </View>
+        )}
+        
+    </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#eaeaea',
+        
+    },
+
+});
+
+export default PatientGeneralInfo
